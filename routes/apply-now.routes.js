7daa9c82b34ae05fd773b8a6ae229d57fd6c70c5
Pramay_07 <76201352+Meet999000@users.noreply.github.com>
2023-03-18 16:45:38 +0530

    feat(superadmin): added superadmin access validation.

--- routes/apply-now.routes.js
+++ routes/apply-now.routes.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/auth");
-const { USER_TYPE: { ADMIN } } = require("../json/enums.json");
+const { USER_TYPE: { ADMIN, SUPERADMIN } } = require("../json/enums.json");
 
 const {
   APPLYNOW: { VALIDATOR, APIS }
 } = require("../controllers");
 
 /* Post Apis */
-router.post("/", auth({ usersAllowed: [ADMIN] }), VALIDATOR.create, APIS.createApplyNow);
-router.post("/get", auth({ usersAllowed: [ADMIN] }), VALIDATOR.fetch, APIS.getApplyNow);
+router.post("/", auth({ usersAllowed: [ADMIN, SUPERADMIN] }), VALIDATOR.create, APIS.createApplyNow);
+router.post("/get", auth({ usersAllowed: [ADMIN, SUPERADMIN] }), VALIDATOR.fetch, APIS.getApplyNow);
 
 /* Put Apis */
-router.put("/update/:_id", auth({ usersAllowed: [ADMIN] }), APIS.updateApplyNow);
+router.put("/update/:_id", auth({ usersAllowed: [ADMIN, SUPERADMIN] }), APIS.updateApplyNow);
 
 /* Delete Apis */
-router.delete("/delete/:_id", auth({ usersAllowed: [ADMIN] }), VALIDATOR.toggleActive, APIS.deleteApplyNow);
+router.delete("/delete/:_id", auth({ usersAllowed: [ADMIN, SUPERADMIN] }), VALIDATOR.toggleActive, APIS.deleteApplyNow);
 
 module.exports = router;
